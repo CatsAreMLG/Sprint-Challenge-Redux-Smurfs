0e0cf325cd5c5f29d0fc68cb6380e37694f2d897
CatsAreMLG <joshuadgon@gmail.com>
2019-02-09 12:29:47 -0600

    Finish MVP

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import { fetchSmurfs } from '../actions'
 import CreateSmurfForm from './createSmurfForm'
 import Smurfs from './Smurfs'
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+
 class App extends Component {
   componentDidMount() {
     this.props.fetchSmurfs()
