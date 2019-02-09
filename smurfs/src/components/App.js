d267b56d45ef7e7ee0b71bd0ad21a9e004b3189e
CatsAreMLG <joshuadgon@gmail.com>
2019-02-09 13:38:37 -0600

    Add loading symbol

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { css } from '@emotion/core';
+import {
+  ScaleLoader
+} from 'react-spinners';
+
+
 import { fetchSmurfs } from '../actions'
 import CreateSmurfForm from './createSmurfForm'
 import UpdateSmurfForm from './updateSmurfForm'
 import Smurfs from './Smurfs'
 import './App.css';
+
+const override = css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    display: block;
+    border-color: #3E61A4;
+`;
 
 class App extends Component {
   componentDidMount() {
@@ -16,7 +31,17 @@ class App extends Component {
       <div className="App">
         <h1>Smurfs Redux</h1>
         {this.props.updatingSmurf ? <div><h2>Editing</h2> <UpdateSmurfForm /></div> : <CreateSmurfForm />}
-        {!this.props.fetchingSmurfs ? <Smurfs /> : <div>LOADING...</div>}
+        {!this.props.fetchingSmurfs
+          ?
+          (<Smurfs />)
+          :
+          (<ScaleLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={'#3E61A4'}
+            loading={this.props.fetchingSmurfs}
+          />)}
       </div>
     );
   }
